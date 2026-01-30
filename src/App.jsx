f967103c0538791cf61d37af1dcae2de57c06534
Punya Mehta <punyadmehta@gmail.com>
2026-01-30 19:54:02 +0530

    hero section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import NavBar from "./Components/NavBar";
+import NavBar from "./Components/navbar";
+import Hero from "./Components/Hero"
 
 const App = () => {
   return (
     <main>
       <NavBar />
+      <Hero />
     </main>
   )
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+
+const Hero = () => {
+  return (
+    <section id="hero">
+        <div>
+            <h1>MacBook Pro</h1>
+            <img src="/title.png" alt="MacBook Title" />
+        </div>
+        <video src="/videos/hero.mp4" autoPlay muted playsInline />
+    </section>
+  )
+}
+
+export default Hero
